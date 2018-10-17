256e330c4479753b5d8b77340abe4cb0783a6215
UtkarshGupta-CS <utgupta1997@gmail.com>
2018-10-17 18:14:31 +0530

    add file upload feature

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,159 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <!--UPLOAD-->
+      <form enctype="multipart/form-data" novalidate v-if="isInitial || isSaving">
+        <h1>Tess Image</h1>
+        <div class="dropbox">
+          <input type="file" :name="uploadFieldName" :disabled="isSaving" @change="filesChange($event.target.name, $event.target.files); fileCount = $event.target.files.length"
+            accept="image/*" class="input-file">
+            <p v-if="isInitial">
+              Drag your file(s) here to begin<br> or click to browse
+            </p>
+            <p v-if="isSaving">
+              Uploading {{ fileCount }} files...
+            </p>
+        </div>
+      </form>
+      <!--SUCCESS-->
+      <div v-if="isSuccess">
+        <h2>Uploaded {{ uploadedFiles.length }} file(s) successfully.</h2>
+        <p>
+          <a href="javascript:void(0)" @click="reset()">Upload again</a>
+        </p>
+        <ul class="list-unstyled">
+          <li v-for="item in uploadedFiles">
+            <img :src="item.url" class="img-responsive img-thumbnail" :alt="item.originalName">
+          </li>
+        </ul>
+      </div>
+      <!--FAILED-->
+      <div v-if="isFailed">
+        <h2>Uploaded failed.</h2>
+        <p>
+          <a href="javascript:void(0)" @click="reset()">Try again</a>
+        </p>
+        <pre>{{ uploadError }}</pre>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Tesseract from "tesseract.js";
 
+import { upload, getImage } from "./file-upload";
+import { wait } from "./utils";
+const STATUS_INITIAL = 0,
+  STATUS_SAVING = 1,
+  STATUS_SUCCESS = 2,
+  STATUS_FAILED = 3;
 export default {
   name: "app",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      uploadedFiles: [],
+      uploadError: null,
+      currentStatus: null,
+      uploadFieldName: "photos"
+    };
+  },
+  computed: {
+    isInitial() {
+      return this.currentStatus === STATUS_INITIAL;
+    },
+    isSaving() {
+      return this.currentStatus === STATUS_SAVING;
+    },
+    isSuccess() {
+      return this.currentStatus === STATUS_SUCCESS;
+    },
+    isFailed() {
+      return this.currentStatus === STATUS_FAILED;
+    }
+  },
+  methods: {
+    reset() {
+      // reset form to initial state
+      this.currentStatus = STATUS_INITIAL;
+      this.uploadedFiles = [];
+      this.uploadError = null;
+    },
+    save(formData) {
+      // const photos = formData.get("photos");
+      // const fReader = new FileReader();
+      // const img = document.createElement("img");
+
+      // fReader.onload = () => {
+      //   img.src = fReader.result;
+      // };
+
+      // fReader.readAsDataURL(photos);
+      // Tesseract.recognize(img)
+      //   .progress(message => console.log(message))
+      //   .catch(err => console.error(err))
+      //   .then(function(result) {
+      //     console.log("s", result);
+      //   });
+      // upload data to the server
+      this.currentStatus = STATUS_SAVING;
+      upload(formData)
+        .then(wait(1500))
+        .then(x => {
+          this.uploadedFiles = [].concat(x);
+          this.currentStatus = STATUS_SUCCESS;
+        })
+        .catch(err => {
+          this.uploadError = err.response;
+          this.currentStatus = STATUS_FAILED;
+        });
+    },
+    filesChange(fieldName, fileList) {
+      // handle file changes
+      const formData = new FormData();
+      if (!fileList.length) return;
+      // append the files to FormData
+      Array.from(Array(fileList.length).keys()).map(x => {
+        this.file = x;
+        formData.append(fieldName, fileList[x], fileList[x].name);
+      });
+      // save it
+      this.save(formData);
+    }
+  },
+  mounted() {
+    this.reset();
   }
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss">
+.dropbox {
+  outline: 2px dashed grey; /* the dash box */
+  outline-offset: -10px;
+  background: lightcyan;
+  color: dimgray;
+  padding: 10px 10px;
+  min-height: 200px;
+  position: relative;
+  cursor: pointer;
+}
+
+.input-file {
+  opacity: 0; /* invisible but it's there! */
+  width: 100%;
+  height: 200px;
+  position: absolute;
+  cursor: pointer;
+}
+
+.dropbox:hover {
+  background: lightblue;
+}
+
+.dropbox p {
+  font-size: 1.2em;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 50px 0;
 }
 </style>
